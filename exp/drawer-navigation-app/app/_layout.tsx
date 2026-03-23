4cafca5f4c03c659674d4d883f2946163d91677d
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-23 20:27:39 +0600

    Bismillah

---
+++ exp/drawer-navigation-app/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Drawer } from 'expo-router/drawer';
+
+export default function RootLayout() {
+  return (
+    <Drawer>
+      <Drawer.Screen name="index" options={{ title: 'Home' }} />
+      <Drawer.Screen name="profile" options={{ title: 'Profile' }} />
+    </Drawer>
+  );
+}

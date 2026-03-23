fc6c203ed1b73e8450dfcd68c32318d1bfef9b0b
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-23 22:49:48 +0600

    Bismillah

--- exp/rnn-drawer/App.tsx
+++ exp/rnn-drawer/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import DrawerNavigator from './app/DrawerNavigator';
+import {StatusBar} from 'expo-status-bar';
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <DrawerNavigator />
-    </GestureHandlerRootView>
-  );
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <StatusBar style="dark"/>
+            <DrawerNavigator/>
+        </GestureHandlerRootView>
+    );
 }

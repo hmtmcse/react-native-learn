fc64b78a8be4b50b28d6b7d6c77dea44f089e759
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-06 12:37:40 +0600

    Woking on mapend trial

--- mapend-trial/application/starter/App.tsx
+++ mapend-trial/application/starter/App.tsx
@@ -1,15 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppConfig from "./src/config/app-config";
+import {Button, StyleSheet, Text, View} from 'react-native';
+import Toast from "react-native-toast-message";
 
 export default function App() {
-  console.log(new AppConfig())
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const showToast = () => {
+        console.log("Tap")
+        Toast.show({
+            type: 'success',
+            text1: 'This is a success message This is a success message This is a success message',
+            topOffset: 35,
+            position: "bottom"
+        });
+        // Toast.show({
+        //     type: 'error',
+        //     text1: 'Hello',
+        //     text2: 'This is some something 👋'
+        // });
+    }
+
+    return (
+        <>
+            <View style={styles.container}>
+                <StatusBar style="auto"/>
+                <Button
+                    title='Show toast'
+                    onPress={showToast}
+                />
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <Toast/>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
